2d18604ba993c73d704f83956e04236e87e85077
Dominik <dt_slomka@op.pl>
2020-03-25 22:39:57 +0100

    PLUSIK

--- IoT/esp8266/src/main/java/pl/esp8266/server/esp8266/repository/EspReadRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.esp8266.server.esp8266.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.esp8266.server.esp8266.model.EspRead;
-
-@Repository
-public interface EspReadRepository extends JpaRepository<EspRead, Long> {
-}
